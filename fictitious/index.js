8eaed8a962f2ac53aca279c833cb51b60e270082
ouyingjie1992 <ouyingjie1992@qq.com>
2018-08-13 10:41:39 +0800

    增加注释

--- fictitious/index.js
+++ fictitious/index.js
@@ -9,40 +9,55 @@
 		var $showPost = $('#showPost');
 		var $postValue = $('#postValue');
 
-		var ajaxPost = function(value){
-			var html = '';
-			html += '<p>';
-			html += '请求接口...参数为value=' + value;
-			html += '</p>';
-		    $showPost.prepend(html);
-		};
+		// 本体，发送请求
+		var ajaxPost = (function(){
+		    return {
+		    	sendPost: function(value){
+					var html = '';
+					html += '<p>';
+					html += '请求接口...参数为value=' + value;
+					html += '</p>';
+				    $showPost.prepend(html);
+			    }
+		    };
+		})();
+		// 代理层
 		var proxyPost = (function(){
 		    var cache = '', 
 		        timer;
-		    return function(value){
-		        if(!timer){
-		            timer = setTimeout(function(){
-		                ajaxPost(cache);
-		                cache = '';
-		                clearTimeout(timer);
-		                timer = null;
-		            }, 1000);
-		        }
-		        cache = value;
+		    return {
+		    	sendPost: function(value){
+			        if(!timer){
+			            timer = setTimeout(function(){
+			                ajaxPost.sendPost(cache); //发送合并后的数据
+			                cache = ''; //清空缓存
+			                clearTimeout(timer); //清除定时器
+			                timer = null;
+			            }, 1000);
+			        }
+			        cache = value; //此处为仅保留最新一类数据，也可改为对象处理多类数据
+			    }
 		    };
 		})();
 
+		// 输入域变动时触发事件
+		var changeFunction = function() {
+			// 调用入口，可无缝抛弃代理处理直接调用本体
+			// ajaxPost.sendPost($postValue.val());
+			proxyPost.sendPost($postValue.val());
+		};
 		$postValue.on('input propertychange', function () {
-			proxyPost($postValue.val());
+			changeFunction();
 		}).on('keydown', function() {
-			proxyPost($postValue.val());
+			changeFunction();
 		}).on('keyup', function() {
-			proxyPost($postValue.val());
+			changeFunction();
 		});
     };
 
     // 请求数据-实现图片预加载
     var initLoadImg = function() {
+    	// 本体，展示图片
 		var addImg = (function(){
 		    var img = document.createElement('img');
 		    document.getElementById('showImg').appendChild(img);
@@ -52,18 +67,21 @@
 		        }
 		    };
 		})();
+		// 代理层
 		var proxyAddImg = (function(){
 		    var img = new Image();
 		    img.onload = function(){
-		        addImg.setSrc(this.src); 
+		        addImg.setSrc(this.src); //图片加载完毕后即刻显示
 		    };
 		    return { 
 		        setSrc: function(src){
-		            addImg.setSrc('img/load.gif'); 
+		            addImg.setSrc('img/load.gif'); //加载loading占位图
 		            img.src = src;
 		        }
 		    };
 		})();
+		// 调用入口，可无缝抛弃代理处理直接调用本体
+		// addImg.setSrc('img/test.jpg'); 
 		proxyAddImg.setSrc('img/test.jpg'); 
     };
 
